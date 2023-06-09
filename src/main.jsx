3d0126a1c0f0683646aaf50033aa6a6ed66e6d0a
KevinPadi <130315691+KevinPadi@users.noreply.github.com>
2023-06-08 23:42:00 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../theme.js'
-import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider theme={theme}>
