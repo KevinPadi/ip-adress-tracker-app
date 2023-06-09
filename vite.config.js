f21ff3abdb6a87bc97433516be76afa2d37f1caf
KevinPadi <130315691+KevinPadi@users.noreply.github.com>
2023-06-08 23:38:58 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: https://kevinpadi.github.io/ip-adress-tracker-app/
+  base: "https://kevinpadi.github.io/ip-adress-tracker-app/"
 })
