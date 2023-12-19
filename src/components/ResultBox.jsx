ece5c73cb093fa07b764bae218e048280c30efe9
Kevin Padilla <kevpadilla01@gmail.com>
2023-12-19 19:35:24 -0300

    fix (ResultBox): stat number component color changed to black

--- src/components/ResultBox.jsx
+++ src/components/ResultBox.jsx
@@ -5,22 +5,22 @@ function ResultBox ({ data }) {
     <Flex flexDirection={['column', 'column', 'row']} position='absolute' bg='white' bottom={['-6.5rem', '-6.5rem', '-5rem']} h={[250, 270, 170]} w={[300, 300, 1200]} zIndex={999} gap={[0, 0, 12]} p={6} borderRadius='xl'>
       <Stat textAlign={['center', 'center', 'left']}>
         <StatLabel fontSize={['10px', '10px', '12px']} color='brand.100' mb={[0, 0, 2]}>IP ADDRESS</StatLabel>
-        <StatNumber fontSize={['lg', 'lg', '2xl']} fontWeight='500'>{data ? data.ip : ''}</StatNumber>
+        <StatNumber fontSize={['lg', 'lg', '2xl']} fontWeight='500' color='black'>{data ? data.ip : ''}</StatNumber>
       </Stat>
       <Divider visibility={['hidden', 'hidden', 'visible']} borderColor='brand.100' orientation='vertical' />
       <Stat textAlign={['center', 'center', 'left']}>
         <StatLabel fontSize={['10px', '10px', '12px']} color='brand.100' mb={[0, 0, 2]}>LOCATION</StatLabel>
-        <StatNumber fontSize={['lg', 'lg', '2xl']} fontWeight='500'>  {data ? `${data.location.region}, ${data.location.country}` : ''}</StatNumber>
+        <StatNumber fontSize={['lg', 'lg', '2xl']} fontWeight='500' color='black'>  {data ? `${data.location.region}, ${data.location.country}` : ''}</StatNumber>
       </Stat>
       <Divider visibility={['hidden', 'hidden', 'visible']} borderColor='brand.100' orientation='vertical' />
       <Stat textAlign={['center', 'center', 'left']}>
         <StatLabel fontSize={['10px', '10px', '12px']} color='brand.100' mb={[0, 0, 2]}>TIMEZONE</StatLabel>
-        <StatNumber fontSize={['lg', 'lg', '2xl']} fontWeight='500'>{data ? `UTC ${data.location.timezone}` : ''} </StatNumber>
+        <StatNumber fontSize={['lg', 'lg', '2xl']} fontWeight='500' color='black'>{data ? `UTC ${data.location.timezone}` : ''} </StatNumber>
       </Stat>
       <Divider visibility={['hidden', 'hidden', 'visible']} borderColor='brand.100' orientation='vertical' />
       <Stat textAlign={['center', 'center', 'left']}>
         <StatLabel fontSize={['10px', '10px', '12px']} color='brand.100' mb={[0, 0, 2]}>ISP</StatLabel>
-        <StatNumber fontSize={['lg', 'lg', '2xl']} fontWeight='500'>{data ? data.isp : ''}</StatNumber>
+        <StatNumber fontSize={['lg', 'lg', '2xl']} fontWeight='500' color='black'>{data ? data.isp : ''}</StatNumber>
       </Stat>
     </Flex>
   // <Flex position='absolute' bg='orange.900' bottom='-4.5rem' h='150px' w='1200px' zIndex={999} gap={12} p={6} borderRadius='xl'>
